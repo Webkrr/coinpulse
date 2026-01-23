97fd51c0231bc2b88b623335766f519c87249184
Webkrr <kingsingh02480@gmail.com>
2026-01-24 01:55:39 +0530

    Initial start of Crypto App

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="main-container inner">
+                <link rel="stylesheet" href="" />
+            </div>
+        </header>
+    )
+}
+export default Header
